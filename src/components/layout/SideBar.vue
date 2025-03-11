6edf191c58ef68247350b87647954e369548ea74
NhuQuynh2503 <nnnhuquynh03@gmail.com>
2025-03-11 22:43:40 +0700

    first commit

---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SidebarMenu from '@/components/layout/sideBar/SidebarMenu.vue'
+import Button from '../ui/button/Button.vue'
+</script>
+<template>
+  <div class="w-18 py-3 flex flex-col w-[250px] min-w-[250px] border-r p-4 gap-2">
+    <p class="font-semibold text-lg mt-5">
+      <span>Q</span>
+      <span>IZZFLY</span>
+    </p>
+    <SidebarMenu />
+    <Button
+      class="mt-auto"
+      @click="logout"
+      >Logout</Button
+    >
+  </div>
+</template>
+<style>
+.router-link-exact-active {
+  background-color: #f7f7f7;
+  color: #344054;
+}
+</style>
---
+++ src/routers/router.ts
@@ -0,0 +1,56 @@
+import { createWebHistory, createRouter, type RouteRecordRaw } from 'vue-router'
+import { authRoute, dashboardRoute, profileRoute, quizRoute } from './modules'
+import { authGuard } from './auth-guard'
+const { progress } = useIndicator()
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    // beforeEnter: [authGuard],
+    component: () => import('@/pages/index.vue'),
+    children: dashboardRoute,
+  },
+  {
+    path: '/auth',
+    meta: {
+      layout: 'GuestLayout',
+      public: true,
+    },
+    beforeEnter: [authGuard],
+    children: authRoute,
+  },
+  {
+    path: '/profiles',
+    // beforeEnter: [authGuard],
+    children: profileRoute,
+  },
+  {
+    path: '/quiz',
+    // beforeEnter: [authGuard],
+    meta: {
+      layout: 'QuizCreateLayout',
+    },
+    children: quizRoute,
+  },
+  {
+    path: '/test',
+    component: () => import('@/pages/test.vue'),
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+router.beforeEach(() => {
+  progress.value = 0.3
+})
+
+router.afterEach(() => {
+  setTimeout(() => {
+    progress.value = 1
+  }, 100)
+})
+
+export default router
